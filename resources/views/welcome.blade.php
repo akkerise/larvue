2af0c814630f09c569cc18fb9b1f34735d90aa5d
akkerise <akkerise@gmail.com>
2018-09-07 01:31:26 +0700

    fixed larvue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,10 @@
 <!doctype html>
-<html lang="{{ app()->getLocale() }}">
+<html lang="{{ config('app.locale') }}">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Lartodo</title>
+        <title>Larvue</title>
         <!-- Fonts -->
         <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
     </head>
